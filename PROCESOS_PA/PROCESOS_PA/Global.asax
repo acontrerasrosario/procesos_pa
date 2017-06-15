844f8b0eb8087b7d3624d73ed5e2db0e844ef5cb
Angel Contreras <acontrerasrosario@gmail.com>
2017-06-15 12:26:39 -0400

    Creating the project MVC ASP.NET

---
+++ PROCESOS_PA/PROCESOS_PA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PROCESOS_PA.MvcApplication" Language="C#" %>
---
+++ PROCESOS_PA/PROCESOS_PA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PROCESOS_PA.Startup))]
+namespace PROCESOS_PA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
