35895834ca9582a9ac24d4602c6e738217f4b1b7
Angel Contreras <acontrerasrosario@gmail.com>
2017-06-19 20:30:36 -0400

    class mother of models

--- procesos_app/procesos_app/Models/MotherOfModels.cs
+++ procesos_app/procesos_app/Models/MotherOfModels.cs
@@ -8,15 +8,15 @@ namespace procesos_app.Models
     public class MotherOfModels
     {
 
-        public Career Careers { get; set; }
-        public Areas Areas { get; set; }
-        public UserCareer UserCareers { get; set; }
-        public Builder Builders { get; set; }
-        public ClassRoom ClassRooms { get; set; }
-        public Section Sections { get; set; }
-        public StudentSection UserSections { get; set; }
-        public Trimester Trimesters { get; set; }
-        public Subject Subjects { get; set; }
-        public Schedule Schedule { get; set; }
+        //public Career Careers { get; set; }
+        //public Areas Areas { get; set; }
+        //public UserCareer UserCareers { get; set; }
+        //public Builder Builders { get; set; }
+        //public ClassRoom ClassRooms { get; set; }
+        //public Section Sections { get; set; }
+        //public StudentSection UserSections { get; set; }
+        //public Trimester Trimesters { get; set; }
+        //public Subject Subjects { get; set; }
+        //public Schedule Schedule { get; set; }
     }
 }
