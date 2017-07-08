bfaa8daf36b712ebfe6f7be762a014db55b27b05
Angel Contreras <acontrerasrosario@gmail.com>
2017-07-08 08:11:30 -0400

    mis avances

---
+++ procesos_app/procesos_app/Controllers/RegistroController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using procesos_app.Models;
+
+namespace procesos_app.Controllers
+{
+    public class RegistroController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult NewSubject()
+        {
+            return View();
+        }
+
+        public ActionResult AdmSubject()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ procesos_app/procesos_app/Scripts/AngularExecute.js
@@ -0,0 +1,22 @@
+﻿(function () {
+
+    var project = angular.module('procesos_pa', ['ngRoute']);
+
+    //project.controller('AddSubjectsController', AddSubjectsController);
+
+
+    var configFunction = function ($routeProvider) {
+        $routeProvider.
+            when('/ModificarSubject/:subjectId', {
+                templateUrl: '/UI/Template/ModSubject.html',
+            })
+           
+    }
+
+    configFunction.$inject = ['$routeProvider'];
+    project.config(configFunction);
+
+
+}());
+
+
